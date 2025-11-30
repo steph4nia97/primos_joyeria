46e5efede6a9e63a05f10a548536687ad41db85f
steph4nia97 <step.munoz@duocuc.cl>
2025-11-30 19:41:27 -0300

    retrofit a docker

--- app/src/main/java/com/primosjoyeria/data/remote/auth/RetroFitCliente.kt
+++ app/src/main/java/com/primosjoyeria/data/remote/auth/RetroFitCliente.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "https://backend-joyeria-wsuf.onrender.com/api/"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
